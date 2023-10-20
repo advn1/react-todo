a5dc5eaabc533a5d1fd5aa754d573d7cf475bc6b
advn1 <135426318+advn1@users.noreply.github.com>
2023-10-20 20:24:10 +0500

    add data file

--- src/Controller.jsx
+++ src/Controller.jsx
@@ -1,8 +1,7 @@
 import {useState, useEffect} from "react"
 import View from "./View.jsx"
 import React from "react"
-import { render } from "react-dom"
-
+import getData from "./Data.js"
 
 export default function Controller() {
   let [todoContent,setTodoContent] = useState('')
@@ -10,36 +9,10 @@ export default function Controller() {
 
   let [count, setCount] = useState(allTodo.length)
   let [todoTypes,setTodoTypes] = useState("all")
-  let [widgets,setWidgets] = useState([])
   let [currentCard, setCurrentCard] = useState()
   let [time, setTime] = useState([new Date().getHours(), new Date().getMinutes()])
 
-  
-
-  let days = {
-    1: "Mon",
-    2: "Tue",
-    3: "Wed",
-    4: "Thu",
-    5: "Fri",
-    6: "Sat",
-    7: "Sun"
-  }
-
-  let months = {
-    1: "Jan",
-    2: "Feb",
-    3:"Mar",
-    4: "Apr",
-    5: "May",
-    6:"Jun",
-    7:"Jul",
-    8: "Aug",
-    9: "Sep",
-    10: "Oct",
-    11 : "Nov",
-    12: "Dec"
-  }
+  let [days,months] = getData()
 
   useEffect(() => {
     const interval = setInterval(() => {
